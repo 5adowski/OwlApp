f46c158ef600c1230ec4afbc613f18a779aa4312
5adowski <wojciechsadowski2@gmail.com>
2024-04-17 22:32:22 +0200

    Added models

--- src/main/java/sadowski/wojciech/owl/app/owlery/Owlery.java
+++ src/main/java/sadowski/wojciech/owl/app/owlery/Owlery.java
@@ -1,4 +1,11 @@
 package sadowski.wojciech.owl.app.owlery;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.UUID;
+
 public class Owlery {
+    private UUID id;
+    private HashSet<UUID> members;
+    private ArrayList<UUID> letters;
 }
