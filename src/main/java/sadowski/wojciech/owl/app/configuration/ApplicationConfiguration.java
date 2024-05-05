5ba12984a3451df7d972a69ed6353f5eaf6211f1
5adowski <wojciechsadowski2@gmail.com>
2024-05-05 21:09:16 +0200

    Added main configuratuin of the application

---
+++ src/main/java/sadowski/wojciech/owl/app/configuration/ApplicationConfiguration.java
@@ -0,0 +1,10 @@
+package sadowski.wojciech.owl.app.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "sadowski.wojciech.owl.app")
+public class ApplicationConfiguration {
+
+}
