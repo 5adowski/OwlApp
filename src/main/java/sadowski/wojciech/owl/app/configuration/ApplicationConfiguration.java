04352ff1ea900dfc8f34cdf7a06935f8921b4f74
5adowski <wojciechsadowski2@gmail.com>
2024-05-21 18:53:37 +0200

    Enabled JPA Repositories via ApplicationConfiguration

--- src/main/java/sadowski/wojciech/owl/app/configuration/ApplicationConfiguration.java
+++ src/main/java/sadowski/wojciech/owl/app/configuration/ApplicationConfiguration.java
@@ -2,9 +2,11 @@ package sadowski.wojciech.owl.app.configuration;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @ComponentScan(basePackages = "sadowski.wojciech.owl.app")
+@EnableJpaRepositories
 public class ApplicationConfiguration {
 
 }
